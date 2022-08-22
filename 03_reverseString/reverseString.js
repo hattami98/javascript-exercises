144985f60c316e9172753ac51714418df78f321a
hattami98 <am.hattami@gmail.com>
2022-08-22 15:45:55 +0700

    Edit reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedStr = '';
+    for(let i = 1; i <= str.length; i++){
+        reversedStr+=str.substr(-i, 1);
+    }
 
+    return reversedStr;
 };
 
 // Do not edit below this line
