25a61e7564560ec15d0fd2347609b792abbf6df0
hattami98 <am.hattami@gmail.com>
2022-08-22 14:20:34 +0700

    Edit repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedStr = string;
+    if(num === 0) return '';
+    else if(num < 0) return 'ERROR';
+    for(let i = 1; i < num; i++){
+        repeatedStr+=string;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
