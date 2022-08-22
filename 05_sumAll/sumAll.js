6163ab3caa316b4d9d6b1b92944db60098ff3dd1
hattami98 <am.hattami@gmail.com>
2022-08-22 18:16:26 +0700

    Edit sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(typeof(a) != 'number' || typeof(b) != 'number' || a < 0 || b < 0) return "ERROR";
+    const range = Math.abs(a-b)+1;
+    return (a+b)*range/2;
 };
 
 // Do not edit below this line
