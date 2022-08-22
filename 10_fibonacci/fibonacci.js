974dcd25914ca5b3d91c7718204af1725bee9984
hattami98 <am.hattami@gmail.com>
2022-08-22 20:10:16 +0700

    Edit fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(max) {
+    max = Number(max);
+    const numbers = [1, 1];
+    if(max < 0) return 'OOPS';
+    for(let i = 2; i < max; i++){
+        numbers[i] = numbers[i-1] + numbers[i-2];
+    }
+    return numbers[max-1];
 };
 
 // Do not edit below this line
