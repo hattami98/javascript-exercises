8143e84e1f974db2df12158d8097196409d243a3
hattami98 <am.hattami@gmail.com>
2022-08-22 18:49:41 +0700

    Edit calculator .js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return Number(a)+Number(b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return Number(a)-Number(b);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let computedSum = 0;
+  for(const number of numbers) computedSum += Number(number);
+  return computedSum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  for(const number of numbers) product *= Number(number);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let product = 1;
+  if(number === 0) return 1;
+  for(let i = 1; i <= number; i++) product *= i;
+  return product;
 };
 
 // Do not edit below this line
