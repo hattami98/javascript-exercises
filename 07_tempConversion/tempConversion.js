843f08fb4fe0b0c806f7e3197236eb54e799283c
hattami98 <am.hattami@gmail.com>
2022-08-22 18:31:37 +0700

    Edit tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+    let celsius = (fahrenheit - 32) / 9 * 5;
+    return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+    let fahrenheit = celsius / 5 * 9 + 32;
+    return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
