e57a48cf33072a0fa83f977de92af82e4149d239
hattami98 <am.hattami@gmail.com>
2022-08-22 17:11:27 +0700

    Edit removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    let filteredArray = [];
+    for(const arrElement of array){
+        let notSame = true;
+        for(const argElement of theArgs){
+            if(arrElement === argElement) notSame = false;
+        }
+        if(notSame) filteredArray.push(arrElement);
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
